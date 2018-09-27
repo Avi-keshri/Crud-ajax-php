dadda3950e0e79b8cd63911f1b7ab7594f42091c
Avi-keshri <kravinash321@gmail.com>
2018-09-27 10:13:49 -0700

    initial commit

--- insert.php
+++ insert.php
@@ -1,4 +1,19 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testing";
 
-print_r($_POST);
-?>
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$contact=$_POST['contact'];
+$sql="INSERT INTO students (name,email,contact) VALUES ('$name','$email','$contact') ";
+$result =mysqli_query($conn, $sql);
+if($result){
+	echo "Data inserted successfully";
+}
+else{
+	echo "something wrong";
+}
